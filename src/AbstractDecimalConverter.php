6983279b8040e2e8069d40bb6d2e4f874bc4bb11
Callum Stotter <carrontiger@users.noreply.github.com>
2024-04-07 13:43:32 +0200

    Comments regarding copyright and license

--- src/AbstractDecimalConverter.php
+++ src/AbstractDecimalConverter.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace carrontiger\Conversion;
 
+/**
+ * Base for conversion from/to decimal numerals.
+ *
+ * @copyright {@link mailto:carrontiger@users.noreply.github.com Callum Stotter}
+ * @license {@link https://www.apache.org/licenses/LICENSE-2.0 Apache-2.0}
+ */
 abstract class AbstractDecimalConverter implements DecimalConverterInterface
 {
     private int   $base;
